79ad3d7fde4d42b8ffaaac7cf6258a612bf46d17
Kang Ki Tae <kt.kang@ridi.com>
2017-01-20 18:13:37 +0900

    Add Ridi Story in product list of payment page.

--- src/Intra/Service/Payment/UserPaymentConst.php
+++ src/Intra/Service/Payment/UserPaymentConst.php
@@ -37,7 +37,7 @@ class UserPaymentConst
     {
         $const = [];
         $const['team'] = UserConstant::$jeditable_key_list['team'];
-        $const['product'] = ['리디북스', '페이퍼샵', '공통'];
+        $const['product'] = ['리디북스', '리디스토리', '페이퍼샵', '공통'];
         $const['tax'] = ['Y', 'N', 'N/A'];
         $const['tax_export'] = ['Y', 'N', 'N/A'];
         $const['is_account_book_registered'] = ['N', 'Y'];
