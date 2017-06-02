Devgrapher/intranet

commit 565f96048728444930c3f30fc3a1848bfb7d2289

Extract team info into .env (#28)
* Extract team info into .env

* Rename team aliases

* Replace delimeter in .env

src/Intra/Service/Payment/UserPaymentMailService.php
@@ -1,7 +1,7 @@
 <?php
 namespace Intra\Service\Payment;
 
-use Intra\Service\User\UserConstant;
+use Intra\Service\User\Organization;
 use Intra\Service\User\UserJoinService;
 use Mailgun\Mailgun;
 
@@ -37,12 +37,12 @@ class UserPaymentMailService
             UserJoinService::getEmailByUidSafe($dto->manager_uid)
         ];
         if ($dto->category == UserPaymentConst::CATEGORY_USER_BOOK_CANCELMENT) {
-            $receivers_append = UserJoinService::getEmailsByTeam(UserConstant::TEAM_CCPQ);
+            $receivers_append = UserJoinService::getEmailsByTeam(Organization::TEAM_CCPQ);
             $receivers = array_merge($receivers, $receivers_append);
             $receivers = array_unique($receivers);
         }
         if ($dto->category == UserPaymentConst::CATEGORY_USER_DEVICE_CANCELMENT) {
-            $receivers_append = UserJoinService::getEmailsByTeam(UserConstant::TEAM_DEVICE);
+            $receivers_append = UserJoinService::getEmailsByTeam(Organization::TEAM_DEVICE);
             $receivers = array_merge($receivers, $receivers_append);
             $receivers = array_unique($receivers);
         }

docs/config.sample.env
@@ -47,3 +47,16 @@ mailgun_api_key =
 mailgun_from =
 
 upload_dir =
+
+teams_aliases_co = 1
+teams_aliases_finance = 2
+teams_aliases_device = 3
+teams_aliases_ccpq = 4
+
+# List team names up to 50.
+teams_1 = '공통 / 경영지원 / HR'
+teams_2 = '공통 / 경영지원 / CASH'
+teams_3 = '디바이스'
+teams_4 = 'ccpq'
+teams_5 = 'books팀'
+teams_6 = 'story팀'

src/Intra/Controller/SupportController.php
@@ -11,7 +11,7 @@ use Intra\Service\Support\SupportFileService;
 use Intra\Service\Support\SupportPolicy;
 use Intra\Service\Support\SupportRowService;
 use Intra\Service\Support\SupportViewDtoFactory;
-use Intra\Service\User\UserConstant;
+use Intra\Service\User\Organization;
 use Intra\Service\User\UserDtoFactory;
 use Intra\Service\User\UserPolicy;
 use Intra\Service\User\UserSession;
@@ -75,7 +75,7 @@ class SupportController implements ControllerProviderInterface
         $columns = SupportPolicy::getColumnFieldsTestUserDto($target, $self);
         $title = SupportPolicy::getColumnTitle($target);
         $const = [
-            'teams' => UserConstant::$jeditable_key_list['team'],
+            'teams' => Organization::readTeamNames(),
             'managers' => UserDtoFactory::createManagerUserDtos(),
             'users' => UserDtoFactory::createAvailableUserDtos(),
         ];
@@ -127,7 +127,7 @@ class SupportController implements ControllerProviderInterface
         foreach ($columns as $column) {
             if ($key == $column->key) {
                 if ($column instanceof SupportColumnTeam) {
-                    foreach (UserConstant::$jeditable_key_list['team'] as $team) {
+                    foreach (Organization::readTeamNames() as $team) {
                         $return[$team] = $team;
                     }
                 } elseif ($column instanceof SupportColumnWorker) {

src/Intra/Controller/UsersController.php
@@ -3,7 +3,7 @@
 namespace Intra\Controller;
 
 use Intra\Model\HolidayModel;
-use Intra\Service\User\UserConstant;
+use Intra\Service\User\Organization;
 use Intra\Service\User\UserDto;
 use Intra\Service\User\UserDtoFactory;
 use Intra\Service\User\UserDtoHandler;
@@ -206,8 +206,8 @@ class UsersController implements ControllerProviderInterface
     public function jeditableKey(Request $request, Application $app)
     {
         $key = $request->get('key');
-        if (UserConstant::$jeditable_key_list[$key]) {
-            $values = UserConstant::$jeditable_key_list[$key];
+        if ($key == 'team') {
+            $values = Organization::readTeamNames();
             $dicts = [];
             foreach ($values as $value) {
                 $dicts[$value] = $value;

src/Intra/Service/Payment/UserPaymentConst.php
@@ -1,7 +1,7 @@
 <?php
 namespace Intra\Service\Payment;
 
-use Intra\Service\User\UserConstant;
+use Intra\Service\User\Organization;
 use Intra\Service\User\UserDtoFactory;
 use Intra\Service\User\UserPolicy;
 use Intra\Service\User\UserSession;
@@ -36,7 +36,7 @@ class UserPaymentConst
     public static function get()
     {
         $const = [];
-        $const['team'] = UserConstant::$jeditable_key_list['team'];
+        $const['team'] = Organization::readTeamNames();
         $const['product'] = ['리디북스', '리디스토리', '페이퍼샵', '공통'];
         $const['tax'] = ['Y', 'N/A'];
         $const['is_account_book_registered'] = ['N', 'Y'];

src/Intra/Service/Payment/UserPaymentService.php
@@ -3,7 +3,7 @@ namespace Intra\Service\Payment;
 
 use Intra\Core\MsgException;
 use Intra\Model\PaymentModel;
-use Intra\Service\User\UserConstant;
+use Intra\Service\User\Organization;
 use Intra\Service\User\UserDto;
 use Intra\Service\User\UserDtoFactory;
 use Intra\Service\User\UserPolicy;
@@ -182,17 +182,18 @@ class UserPaymentService
             }
         } else {
             $payment_dicts = $this->payment_model->getPayments($uid, $month);
-            if ($self->team == UserConstant::TEAM_HUMAN_MANAGE && !UserPolicy::isTa($self)) {
+            if ($self->team == Organization::getTeamName(Organization::ALIAS_CO)
+                && !UserPolicy::isTa($self)) {
                 $payment_dicts_append = $this->payment_model->getPaymentsWithOption($month, ['category' => [UserPaymentConst::CATEGORY_ASSETS, UserPaymentConst::CATEGORY_WELFARE_EXPENSE]]);
                 $payment_dicts = array_merge($payment_dicts, $payment_dicts_append);
                 $payment_dicts = array_unique($payment_dicts, SORT_REGULAR);
             }
-            if ($self->team == UserConstant::TEAM_CCPQ) {
+            if ($self->team == Organization::getTeamName(Organization::ALIAS_CCPQ)) {
                 $payment_dicts_append = $this->payment_model->getPaymentsWithOption($month, ['category' => [UserPaymentConst::CATEGORY_USER_BOOK_CANCELMENT]]);
                 $payment_dicts = array_merge($payment_dicts, $payment_dicts_append);
                 $payment_dicts = array_unique($payment_dicts, SORT_REGULAR);
             }
-            if ($self->team == UserConstant::TEAM_DEVICE) {
+            if ($self->team == Organization::getTeamName(Organization::ALIAS_DEVICE)) {
                 $payment_dicts_append = $this->payment_model->getPaymentsWithOption($month, ['category' => [UserPaymentConst::CATEGORY_USER_DEVICE_CANCELMENT]]);
                 $payment_dicts = array_merge($payment_dicts, $payment_dicts_append);
                 $payment_dicts = array_unique($payment_dicts, SORT_REGULAR);

src/Intra/Service/Support/SupportPolicy.php
@@ -23,7 +23,7 @@ use Intra\Service\Support\Column\SupportColumnTeam;
 use Intra\Service\Support\Column\SupportColumnText;
 use Intra\Service\Support\Column\SupportColumnTextDetail;
 use Intra\Service\Support\Column\SupportColumnWorker;
-use Intra\Service\User\UserConstant;
+use Intra\Service\User\Organization;
 use Intra\Service\User\UserDto;
 use Intra\Service\User\UserJoinService;
 
@@ -186,10 +186,10 @@ class SupportPolicy
         ];
 
         $is_human_manage_team = function (UserDto $user_dto) {
-            return $user_dto->team == UserConstant::TEAM_HUMAN_MANAGE;
+            return $user_dto->team == Organization::getTeamName(Organization::ALIAS_CO);
         };
         $is_cash_flow_team = function (UserDto $user_dto) {
-            return $user_dto->team == UserConstant::TEAM_CASH_FLOW;
+            return $user_dto->team == Organization::getTeamName(Organization::ALIAS_FINANCE);
         };
         $get_team_by_uid = function (SupportDto $support_dto) {
             $uid = $support_dto->dict['uid'];

src/Intra/Service/User/Organization.php
@@ -1,0 +1,35 @@
+<?php
+
+namespace Intra\Service\User;
+
+class Organization
+{
+    const MAX_TEAM_ID = 50;
+
+    // Predefined aliases
+    const ALIAS_CO = 'co';
+    const ALIAS_FINANCE = 'finance';
+    const ALIAS_DEVICE = 'device';
+    const ALIAS_CCPQ = 'ccpq';
+
+    public static function readTeamNames()
+    {
+        $team_names = [];
+        foreach (range(1, self::MAX_TEAM_ID) as $id) {
+            if (!empty($_ENV["teams_$id"])) {
+                array_push($team_names, $_ENV["teams_$id"]);
+            }
+        }
+        return $team_names;
+    }
+
+    public static function getTeamName($alias)
+    {
+        if (!empty($_ENV["teams_aliases_$alias"])) {
+            $id = $_ENV["teams_aliases_$alias"];
+            return $_ENV["teams_$id"];
+        } else {
+            return "";
+        }
+    }
+}

commit b20630312fc0ee3d50e85ce17bca03d2cfbf3537

Fix payment email sending error
- A regression introduced by 565f9604.

src/Intra/Service/Payment/UserPaymentMailService.php
@@ -37,12 +37,12 @@ class UserPaymentMailService
             UserJoinService::getEmailByUidSafe($dto->manager_uid)
         ];
         if ($dto->category == UserPaymentConst::CATEGORY_USER_BOOK_CANCELMENT) {
-            $receivers_append = UserJoinService::getEmailsByTeam(Organization::TEAM_CCPQ);
+            $receivers_append = UserJoinService::getEmailsByTeam(Organization::ALIAS_CCPQ);
             $receivers = array_merge($receivers, $receivers_append);
             $receivers = array_unique($receivers);
         }
         if ($dto->category == UserPaymentConst::CATEGORY_USER_DEVICE_CANCELMENT) {
-            $receivers_append = UserJoinService::getEmailsByTeam(Organization::TEAM_DEVICE);
+            $receivers_append = UserJoinService::getEmailsByTeam(Organization::ALIAS_DEVICE);
             $receivers = array_merge($receivers, $receivers_append);
             $receivers = array_unique($receivers);
         }