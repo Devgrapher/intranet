e9bf1f557729a66d39875a35c629d085180a0838
Kang Ki Tae <kt.kang@ridi.com>
2017-06-13 17:37:29 +0900

    Fix typo.

--- src/Intra/Service/Support/SupportDinnerService.php
+++ src/Intra/Service/Support/SupportDinnerService.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 class SupportDinnerService
 {
     const TIME_DINNER_ORDER_START = '15:00';
-    const TIME_DINNER_ORDER_END = '17:30';
+    const TIME_DINNER_ORDER_END = '17:40';
     const MSG_DINNER_ORDER_NOT_EXISTS = '가능한 저녁주문이 없습니다. 관리자에게 문의해주세요.';
     const MSG_DINNER_ORDER_NOT_AVAILABLE = '저녁주문은 평일 오후 3시부터 5시40분까지 신청 가능합니다.';
 
