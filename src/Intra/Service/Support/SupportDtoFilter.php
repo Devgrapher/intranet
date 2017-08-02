d0150c67d25d119c4900a022b4a40788efc33606
Jihun Kim <devgrapher@ridi.com>
2017-08-02 15:35:17 +0900

    Move support adding filters into SupportPolicy

--- src/Intra/Service/Support/SupportDtoFilter.php
+++ src/Intra/Service/Support/SupportDtoFilter.php
@@ -61,76 +61,7 @@ class SupportDtoFilter
             }
         }
 
-        if ($support_dto->target == SupportPolicy::TYPE_FAMILY_EVENT) {
-            $category = $support_dto->dict[$columns['분류']->key];
-
-            if ($category == '결혼') {
-                $flower_type_column = '화환';
-            } elseif (in_array($category, ['자녀출생', '졸업', '장기근속(3년)'])) {
-                $flower_type_column = '과일바구니';
-            } elseif (in_array($category, ['사망-형제자매 (배우자 형제자매포함)', '사망-부모 (배우자 부모 포함)', '사망-조부모 (배우자 조부모 포함)'])) {
-                $flower_type_column = '조화';
-            } else {
-                $flower_type_column = '기타';
-            }
-
-            if ($support_dto->dict[$columns['대상자']->key] == '외부') {
-                if ($support_dto->dict[$columns['화환 종류']->key] != '기타') {
-                    throw new MsgException('대상자가 외부일 경우, 화환 종류를 기타로 선택 후 직접 입력해주세요.');
-                }
-            } else {
-                if ($support_dto->dict[$columns['화환 종류']->key] == '자동선택') {
-                    $support_dto->dict[$columns['화환 종류']->key] = $flower_type_column;
-                }
-            }
-
-            if (in_array(
-                $category,
-                [
-                    '결혼',
-                    '자녀출생',
-                    '사망-부모 (배우자 부모 포함)',
-                ]
-            )) {
-                $cash = '1000000';
-                $support_dto->dict[$columns['경조금']->key] = $cash;
-            }
-
-            $flower_datetime = trim($support_dto->dict[$columns['화환 도착일시']->key]);
-            $flower_datetime_parsed = date_create($flower_datetime . ':00');
-            if ($flower_datetime_parsed === false) {
-                throw new MsgException('화환 도착일시를 다시 확인해주세요');
-            }
-        } elseif ($support_dto->target == SupportPolicy::TYPE_BUSINESS_CARD) {
-            if ($support_dto->dict[$columns['제작(예정)일']->key] == '') {
-                $support_dto->dict[$columns['제작(예정)일']->key] = date("Y-m-t");
-            }
-        } elseif ($support_dto->target == SupportPolicy::TYPE_DEPOT) {
-            $request_date = $support_dto->dict[$columns['구매예정일']->key];
-            $request_datetime = date_create($request_date);
-            if ($request_datetime === false) {
-                throw new MsgException('날짜입력을 다시 확인해주세요');
-            }
-        } elseif ($support_dto->target == SupportPolicy::TYPE_GIFT_CARD_PURCHASE) {
-            if ($support_dto->dict[$columns['신청매수']->key] <= 0) {
-                throw new MsgException('신청 매수와 금액을 확인해주세요');
-            }
-            if (empty($support_dto->dict[$columns['입금자명']->key])) {
-                throw new MsgException('입금자명을 입력해주세요');
-            }
-            if ($support_dto->dict[$columns['신청매수']->key] < $support_dto->dict[$columns['봉투수량']->key]) {
-                throw new MsgException('봉투수량은 최대 신청매수까지 입력할 수 있습니다.');
-            }
-            $input_due = $support_dto->dict[$columns['입금예정일시(24시간 내)']->key];
-            $max_due = date('Y/m/d H:i', strtotime('+1 day'));
-            if ($input_due > $max_due) {
-                throw new MsgException('입금예정일시는 24시간내로 설정하여 주세요');
-            }
-        } elseif ($support_dto->target == SupportPolicy::TYPE_TRAINING) {
-            if (empty($support_dto->dict[$columns['수강료']->key])) {
-                throw new MsgException('수강료를 입력해주세요');
-            }
-        }
+        SupportPolicy::validateFieldsOnAdd($support_dto);
 
         $support_dto->dict['uuid'] = self::getUuidHeader($support_dto->target);
 
