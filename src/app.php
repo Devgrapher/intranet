b49925889cfa9c541efbf7cb044b998681e2b33e
Kang Ki Tae <kt.kang@ridi.com>
2017-11-17 19:18:54 +0900

    Remove IntraAppcation, and divide it to app.php and controller.php.

---
+++ src/app.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+use Intra\Core\WebpackManifestVersionStrategy;
+use Intra\Service\Menu\MenuService;
+use Silex\Application;
+use Silex\Provider\TwigServiceProvider;
+use Symfony\Component\Asset\PathPackage;
+
+$app = new Application([
+    'debug' => $_ENV['INTRA_DEBUG'] ?? false,
+    'asset_manifest_path' => __DIR__ . '/../web/static/dist/manifest.json',
+    'asset_public_path' => '/static/dist',
+]);
+
+$app->register(new TwigServiceProvider(), [
+    'twig.options' => [
+        'cache' => __DIR__ . '/../var/cache',
+        'auto_reload' => true,
+    ],
+    'twig.path' => [
+        __DIR__ . '/../assets/views',
+    ],
+]);
+
+$app->extend('twig', function (Twig_Environment $twig) use ($app) {
+    $version_strategy = new WebpackManifestVersionStrategy($app['asset_manifest_path']);
+    $asset_package = new PathPackage($app['asset_public_path'], $version_strategy);
+    $twig->addFunction(new Twig_SimpleFunction('asset', function ($asset_name) use ($asset_package) {
+        return $asset_package->getUrl($asset_name);
+    }));
+
+    $menu_list = MenuService::getMenuLinkList();
+    $twig->addGlobal('leftMenuList', $menu_list['left']);
+    $twig->addGlobal('rightMenuList', $menu_list['right']);
+    $twig->addGlobal('globalDomain', $_ENV['INTRA_DOMAIN']);
+    $twig->addGlobal('sentryPublicKey', $_ENV['SENTRY_PUBLIC_KEY']);
+
+    $twig->addFunction(new Twig_SimpleFunction('printMenuList', function ($menu_list) {
+        $html = '';
+        if (is_array($menu_list)) {
+            foreach ($menu_list as $menu) {
+                $html .= $menu->getHtml();
+            }
+        } else {
+            $html = $menu_list->getHtml();
+        }
+        return new Twig_Markup($html, 'UTF-8');
+    }));
+
+    return $twig;
+});
+
+return $app;
---
+++ src/controller.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+use Intra\Service\User\UserPolicy;
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+$app->before(function (Request $request) {
+    return UserPolicy::assertRestrictedPath($request);
+});
+
+$app->get('/', function (Application $app) {
+    $subRequest = Request::create('/posts/notice', 'GET');
+    return $app->handle($subRequest, HttpKernelInterface::SUB_REQUEST);
+});
+
+$app->mount('/api', new Intra\Controller\APIController());
+$app->mount('/holidays', new Intra\Controller\HolidaysController());
+$app->mount('/organization', new Intra\Controller\OrganizationController());
+$app->mount('/payments', new Intra\Controller\PaymentsController());
+$app->mount('/posts', new Intra\Controller\PostsController());
+$app->mount('/press', new Intra\Controller\PressController());
+$app->mount('/programs', new Intra\Controller\ProgramsController());
+$app->mount('/receipts', new Intra\Controller\ReceiptsController());
+$app->mount('/users', new Intra\Controller\UsersController());
+$app->mount('/usersession', new Intra\Controller\UserSessionController());
+$app->mount('/weekly', new Intra\Controller\WeeklyController());
+$app->mount('/flextime', new Intra\Controller\FlexTimeController());
+$app->mount('/holidayadmin', new Intra\Controller\HolidayAdminController());
+$app->mount('/rooms', new Intra\Controller\RoomsController());
+$app->get('/focus/', function (Application $app) {
+    $subRequest = Request::create('/rooms/', 'GET', ['type' => 'focus']);
+    return $app->handle($subRequest, HttpKernelInterface::SUB_REQUEST);
+});
+
+$app->mount('/support', new Intra\Controller\SupportController());
+$app->mount('/admin', new Intra\Controller\AdminController());
