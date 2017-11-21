327750428cb06fca56e420af485693af41d77ba8
Jihun Kim <devgrapher@ridi.com>
2017-11-21 16:53:45 +0900

    Fix sample.env path

--- bin/init_sample_db.sh
+++ bin/init_sample_db.sh
@@ -2,7 +2,7 @@
 set -e
 
 composer install
-cp .env.sample_ .env
+cp .env.sample .env
 mysql -uroot -h 127.0.0.1 -e "create database intranet;"
 vendor/bin/phinx migrate -e local -v
 vendor/bin/phinx seed:run -s Teams -s Users -s Rooms -s Policies -s Recipients -e local -v
