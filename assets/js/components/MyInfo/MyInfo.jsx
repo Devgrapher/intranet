4a8279eb693e2d5e047e3dedfa9abc41c4519785
krazijames <krazijames@ridi.com>
2018-01-17 16:49:32 +0900

    Removed unused files.

--- assets/js/components/MyInfo/MyInfo.jsx
+++ assets/js/components/MyInfo/MyInfo.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import axios from 'axios';
 import Dropzone from 'react-dropzone';
 import cn from 'classnames';
-import '../../../css/myInfo.css';
 
 class MyInfo extends React.Component {
   constructor(props) {
